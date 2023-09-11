90417d9a71f2f1dab472b1432ce67c2b7dff96e7
Jonathan Brink <jonathandavidbrink@gmail.com>
2023-09-11 14:23:32 -0400

    remove reference

--- lorums/lorum.pl
+++ lorums/lorum.pl
@@ -7,10 +7,10 @@ use File::Spec;
 # __lib__ will be replaced with the determined library of the file
 my %taskHash=(
     # .js file that is NOT qunit file
-    '.*\/openuilibs\/sas.*\/src\/.*(?<!\.qunit)\.js$' => "copy:src-target-__lib__ copy:npm-package",
+    '.*\/src\/.*(?<!\.qunit)\.js$' => "copy:src-target-__lib__ copy:npm-package",
 
     # .js file that is NOT qunit file that has "test" after it's lib
-    '.*\/openuilibs\/sas.*\/test\/.*(?<!\.qunit)\.js$' => "copy:test-target-__lib__ copy:npm-package",
+    '.*\/*\/test\/.*(?<!\.qunit)\.js$' => "copy:test-target-__lib__ copy:npm-package",
 
     # qunit js or html file
     '\.qunit\.(html|js)$' => "copy:test-target-__lib__"
